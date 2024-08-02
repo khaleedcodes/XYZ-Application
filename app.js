5b32fb7d48293411f5c3b7d2f9b0e696f970d767
AJ <ajmalthedealcloser@gmail.com>
2024-08-02 19:20:37 -0400

    Fixed issue with database connection

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Start server
+// Start server on port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
